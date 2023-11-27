1ff6da4b82c7f7c89e1ad6d6623fe40525c901fe
STSumanBhandari <suman.bhandari@shridharatechnologies.in>
2023-11-27 20:56:25 +0530

    forgot password functionality

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,12 +1,12 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const config = {
-    PORT: process.env.PORT || 5000,
-    MONGO_DB_URL:process.env.MONGO_DB_URL
-
-}
+  PORT: process.env.PORT || 5000,
+  MONGO_DB_URL: process.env.MONGO_DB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
+};
 
 export default config;
-
--- backend/src/models/user.schema.js
+++ backend/src/models/user.schema.js
@@ -1,46 +1,80 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles.js";
-import bcrypt from 'bcryptjs';
-const userSchema = new mongoose.Schema({
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import config from "../config/index.js";
+import crypto from "crypto";
+
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: ["true", "Name is required"],
-        maxLength: [50, "Name must be less than 50 chars"]
+      type: String,
+      required: ["true", "Name is required"],
+      maxLength: [50, "Name must be less than 50 chars"],
     },
     email: {
-        type: String,
-        required: ["true", "Email is required"],
+      type: String,
+      required: ["true", "Email is required"],
     },
     password: {
-        type: String,
-        required: [true, "Password is required"],
-        minLength: [8, "password must be at least 8 chars"],
-        select: false
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "password must be at least 8 chars"],
+      select: false,
     },
     role: {
-        type: String,
-        enum: Object.values(AuthRoles),
-        default: AuthRoles.USER
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
     },
     forgotPasswordToken: String,
-    forgotPasswordExpiry: Date
-}, {timestamps: true})
-
+    forgotPasswordExpiry: Date,
+  },
+  { timestamps: true }
+);
 
 // Encrypt the password before saving
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next()
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-    
-})
-
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 userSchema.methods = {
-    comparePassword: async function (enteredPassword) {
-        return await bcrypt.compare(enteredPassword,this.password) 
+  //compare Password
+  comparePassword: async function (enteredPassword) {
+    try {
+      return await bcrypt.compare(enteredPassword, this.password);
+    } catch (error) {
+      console.log("Error in comparing password");
     }
-}
+  },
+  // generate JWT token
+  getJWTtoken: function () {
+    JWT.sign({ _id: this._id, role: this.role }, config.JWT_SECRET, {
+      expiresIn: config.JWT_EXPIRY,
+    });
+  },
 
-export default mongoose.model("User", userSchema)
+  //generate forgot password token
+  generateForgotPasswordToken: function () {
+    const forgotToken = crypto.randomBytes(20).toString("hex");
+
+    //to encrypt the token which is generate by the crypto
+    this.forgotPasswordToken = crypto
+      .createHash("sha256")
+      .update(forgotToken)
+      .digest("hex");
+
+    //time for token to expire
+
+    this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000;
+
+    return forgotToken;
+  },
+};
+
+export default mongoose.model("User", userSchema);
+
+// 2:33
